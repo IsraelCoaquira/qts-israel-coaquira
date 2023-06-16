7a87dead770a956b0e74ff9e8d48a0fc7147d6d1
IsraelCoaquira <israel.cojum@gmail.com>
2023-06-16 08:34:54 -0300

    Aula 3 de QTS - testes de asserts

--- TDD/testeapi_test.js
+++ TDD/testeapi_test.js
@@ -6,9 +6,20 @@ import {
 
 let aluno = 'Cróvis José'
 let professores = ['João', 'Lais', 'Davi', 'Bruno Aparecido Cano']
+let numeros = [11, 24, 69]
 
 Deno.test("teste de assertions (API)", () => { 
     assertEquals(aluno, "Cróvis José");
     assertStringIncludes(aluno, "José");
 
+})
+
+Deno.test("Testando números", () =>{
+    assertArrayIncludes(numeros,[8, 24, 33])
+})
+
+Deno.test("Testando array do professor (API)", () => {
+    assertArrayIncludes(professores,
+        ["Davi", "Lais", "Alan"],
+        "Opa algo deu errado!");
 })
